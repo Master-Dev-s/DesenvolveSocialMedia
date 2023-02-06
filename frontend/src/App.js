14058f06d9307faa30a2589929bf6fd92252091f
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-02-06 16:57:08 -0300

    setup inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  const get = async () => {
+    const response = await fetch("http://localhost:8000");
+    console.log(response);
+  };
+  get();
+  return <div>welcome to frontend</div>;
+}
+
+export default App;
